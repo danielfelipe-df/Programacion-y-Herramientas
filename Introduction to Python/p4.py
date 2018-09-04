8f5d7cac082c823c36316fc4833d747c58e2adfb
Cristian David Herrera <cdherreram@unal.edu.co>
2018-09-04 15:36:43 -0500

    Ejercicios clase

---
+++ Introduction to Python/p4.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+import math
+print "{:5.3f}".format(math.pi)
+print "{:9.3f}".format(math.pi)
---
+++ Introduction to Python/p5.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+a,b,c=input("Ponga 3 numeros (separados por comas): ")
+print a,b,c
+w=raw_input("Teclee alguna cosa: ")
+print w
+print w[::-1]
