836dfbff89bcf48a7bfe6e1781f954af6ee7cdfd
Daniel Ortiz <dfortizc@unal.edu.co>
2018-09-08 19:52:31 -0500

    Segundo punto del taller

---
+++ Taller 2/Ejercicio2.py
@@ -0,0 +1,40 @@
+from turtle import *
+from math import *
+
+setup(640,480,0,0)
+title("Ejercicio1")
+
+def ir(x,y): #Esta función manda, sin rayar, a la tortuga a una posición
+    penup()
+    goto(x,y)
+    pendown()
+
+def poligono2(x,n2,l2,teta2): #Forma un polígono de m lados con longitudes 2a
+    while(n2<p2):
+        forward(-2*x)
+        left(teta2)
+        n2+=1
+
+def poligono1(x,n1,l1): #Ayuda a formar un cuadrado de m lados con longitudes 2a
+    while(n1<l1):
+        left(90*n1)        
+        forward(-2*x)
+        n1+=1
+
+l2=input("Número de lados del polígono: ")
+
+l1=4;  n1=0;  xy1=-100;  
+p2=int(l2);  teta2=360/p2;  beta2=(p2-2)*180/p2;   xy2=-30;  
+d2=2*xy2*abs(sin(radians(beta2/2))/sin(radians(teta2)));
+h2=d2*abs(sin(radians(beta2/2)))  
+
+ir(xy1,xy1)
+while(n1<l1):
+    ir(xcor()+xy2,ycor()+h2)
+    right(heading())
+    n2=0
+    poligono2(xy2,n2,l2,teta2)
+    ir(xcor()-xy2,ycor()-h2)
+    penup()
+    poligono1(xy1,n1,1+n1)
+    n1+=1
